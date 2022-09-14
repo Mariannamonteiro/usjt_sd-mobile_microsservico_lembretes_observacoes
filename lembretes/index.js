f7b4f7de0144a61eb9e7d37cc2fba30553757cda
Mariannamonteiro <mamonteiro.oliveira@gmail.com>
2022-09-13 21:42:49 -0300

    apostila03: topico 4.3.15 concluida - 13 set

--- lembretes/index.js
+++ lembretes/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const axios = require('axios')
 //estamos aplicando um middleware
 app.use(express.json())
 
@@ -14,12 +15,26 @@ app.get('/lembretes',(req, res) => {
 
 //GET obter a lista de lembretes
 //localhost:40000/lembretes
-app.post('/lembretes', (req, res) => {
+app.post('/lembretes', async(req, res) => {
     contador++
     const texto = req.body.texto
     lembretes[contador] = {contador, texto}
-    res.status(201).end()
 
+    await axios.post('http://localhost:10000/eventos', {
+        tipo: "Lembrete Criado",
+        dados:{
+            contador,
+            texto
+        }
+    })
+
+    res.status(201).send(lembretes[contador])
+
+})
+
+app.post('/eventos', (req, res) =>{
+    console.log(req.body)
+    res.status(200).send({msg: 'ok'})
 })
 
 app.listen(4000, () => console.log('Lembretes. Porta 4000'))
--- observacoes/index.js
+++ observacoes/index.js
@@ -1,19 +1,43 @@
-// express é um framework web para o node que manipula requisições HTTP
-const express = require('express')
+const express = require ('express')
 const app = express()
+const axios = require('axios');
+//erro comum: esquecer de chamar a função
+//json 
+//não faça isso: app.use(express.json)
 app.use(express.json())
 
-const observacoes = {}
-let contador = 0
+const {v4 : uuidv4} = require('uuid');
 
-app.post('/lembretes/:id/observacoes', (req, resp) => {
-    
+const observacoesPorLembreteId = {};
 
+//localhost:5000/lembretes/123456/observacoes
+app.post('/lembretes/:id/observacoes', async (req, res) => {
+    const idObs = uuidv4();
+    const { texto } = req.body;
+    //req.params dá acesso à lista de parâmetros da URL
+    const observacoesDoLembrete = 
+        observacoesPorLembreteId[req.params.id] || [];
+    observacoesDoLembrete.push({id: idObs, texto});
+    observacoesPorLembreteId[req.params.id] = observacoesDoLembrete;
+    await axios.post('http://localhost:10000/eventos', {
+        tipo: 'ObservacaoCriada',
+        dados: {
+            id: idObs,
+            texto,
+            lembreteId: req.params.id
+        }
+    })
+    res.status(201).send(observacoesDoLembrete);
 })
 
-app.get('/lembretes/:id/observacoes', (req, resp) => {
-
+//localhost:5000/lembretes/abcd/observacoes
+app.get('/lembretes/:id/observacoes', (req, res) => {
+    res.send(observacoesPorLembreteId[req.params.id] || []);
 })
 
-// sempre colocar no final, se não o serviço não enxerga os metodos HTTP
-app.listen(5000, () => console.log('Observacoes. Porta 5000'))
+app.post('/eventos', (req, res) => {
+    console.log(req.body);
+    res.status(200).send({msg: 'ok'});
+  })
+  
+app.listen(5000, () => console.log('Observações. Porta 5000.'))
